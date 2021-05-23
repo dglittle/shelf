07866522c570cba8761223c09bfafcf2e53d67c7
My Name <glittle@gmail.com>
2021-05-22 21:09:34 -0700

    bloop

--- index.js
+++ index.js
@@ -50,7 +50,11 @@ var shelf = {}
                 }
             }
         } else {
-            if (a[1] == null || (a[1] < 0 && b[1] == null) || b[1] > a[1] || (b[1] == a[1] && greater_than(b[0], a[0]))) {
+            if (b[1] && b[1].t == 'add') {
+                change = [a[0] + b[0], a[1] + 1]
+                a[0] = change[0]
+                a[1] = change[1]
+            } else if (a[1] == null || (a[1] < 0 && b[1] == null) || b[1] > a[1] || (b[1] == a[1] && greater_than(b[0], a[0]))) {
                 change = [b[0], b[1] ?? 0]
                 a[0] = change[0]
                 a[1] = change[1]
